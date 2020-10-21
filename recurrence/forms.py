bdec96ae148b33c2239df67f791337857ade4e3b
Grzegorz Ślusarek <slusarek@cic.com>
2020-10-21 09:11:46 +0200

    GST-2744 adjust code to work with Django 2.2

--- recurrence/forms.py
+++ recurrence/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.conf import settings
-from django.core import urlresolvers
+from django.urls import reverse, NoReverseMatch  # adjust code to work with django 2.2
 from django.views import i18n
 from django.utils import safestring
 from django.utils.translation import ugettext_lazy as _, get_language
@@ -205,9 +205,9 @@ def find_recurrence_i18n_js_catalog():
 
     # first try to use the dynamic form of the javascript_catalog view
     try:
-        return urlresolvers.reverse(
+        return reverse(
             i18n.javascript_catalog, kwargs={'packages': 'recurrence'})
-    except urlresolvers.NoReverseMatch:
+    except NoReverseMatch:
         pass
 
     # then scan the entire urlconf for a javascript_catalague pattern
@@ -221,9 +221,9 @@ def find_recurrence_i18n_js_catalog():
             elif (pattern.callback == i18n.javascript_catalog and
                   'recurrence' in pattern.default_args.get('packages', [])):
                 if pattern.name:
-                    return urlresolvers.reverse(pattern.name)
+                    return reverse(pattern.name)
                 else:
-                    return urlresolvers.reverse(pattern.callback)
+                    return reverse(pattern.callback)
 
     root_urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
     url = check_urlpatterns(root_urlconf.urlpatterns)
